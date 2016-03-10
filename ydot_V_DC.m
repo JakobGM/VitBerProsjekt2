5b15be33e9311d1d898275eb7ea0a96aee8f165a
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2016-03-10 13:02:56 +0100

    Cleanup for readability

--- ydot_V_DC.m
+++ ydot_V_DC.m
@@ -1,22 +1,26 @@
-function z=ydot_V_DC(t,y)
+function z = ydot_V_DC(t,y)
 % Right-hand side of particle ODE's from oppg3
+% Input (t,y), where t is the time and y = [x, y, u, v]
+% Returns vector z = [u v a_x a_y]
+
 % constants
 V_DC = 5; % DC-voltage
-m = 28*1.66*10^(-27); % particle mass
-q = 1.60*10^(-19); % particle charge
-r_0 = 3*10^(-3); % electrode distance to origo
+u = 1.66054e-27; % 1 atomic mass unit
+m = 28*u; % particle mass
+q = 1.60217646e-19; % particle charge
+r_0 = 3e-3; % electrode distance to origo
 
 % positions
 x_pos = y(1);
 y_pos = y(2);
 
-% e-field components
-E_x = -V_DC*2*x_pos/r_0^2;
-E_y =  V_DC*2*y_pos/r_0^2;
-
 % velocities
 u = y(3);
 v = y(4);
+
+% e-field components
+E_x = -V_DC*2*x_pos/r_0^2;
+E_y =  V_DC*2*y_pos/r_0^2;
 
 % form the equations
 z  = zeros(1,4);
