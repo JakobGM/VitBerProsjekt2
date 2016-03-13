75f683438a2c59175a2af5f2eb03036f3e348d74
Erik Hide Sæternes <erik.hs.93@gmail.com>
2016-03-13 14:23:34 +0100

    Changes to plotStableUnstable.m

--- plotStableUnstable.m
+++ plotStableUnstable.m
@@ -1,12 +1,12 @@
 function plotStableUnstable()
 
 V_AC = 5; % AC-voltage
-step_V_AC = 0.5; % step size V_AC
-step_V_DC = 0.5; % step size V_DC
+step_V_AC = 0.05; % step size V_AC
+step_V_DC = 0.05; % step size V_DC
 values = []; % line between stable and unstable
 T = 0.00002; % total time
 h = 1e-7; % step size for numeric method (RK4)
-ic = [0.001, 0.001]; % initial conditions (x,y)
+ic = [0.001, 0.001, 0, 0]; % initial conditions (x,y)
 u = 1.66054e-27; % atomic mass unit
 m = 28*u; % particle mass
 
@@ -31,6 +31,7 @@ for V_DC = 1 : step_V_DC : 10
 
 end
 
-
+plot(values(:,1), values(:,2), 'k');
+xlabel('V_AC','FontSize',fs); ylabel('V_DC','FontSize',fs);
 
 end
