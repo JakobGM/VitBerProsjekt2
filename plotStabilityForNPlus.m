b9506b0de64071c74e831d00a2d1141dfe3afc6d
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2016-03-14 16:31:26 +0100

    Style change and fixing labels on figures

--- plotStabilityForNPlus.m
+++ plotStabilityForNPlus.m
@@ -5,14 +5,15 @@ step_V_AC = 0.2; % step size V_AC
 step_V_DC = 0.2; % step size V_DC
 values = []; % line between stable and unstable
 T = 0.00002; % total time
-h = 10^(-8); % step size for numeric method (RK4)
+h = 1e-8; % step size for numeric method (RK4)
 ic = [0.001, 0.001, 0, 0]; % initial conditions (x,y)
 u = 1.66054e-27; % atomic mass unit
 m = 14*u; % particle mass
 
 i = 1;
-for V_DC = 2 : step_V_DC : 4.5
+for V_DC = 2:step_V_DC:4.5
     stable = getTrajectoryStability(T, h, ic, V_DC, V_AC, m);
+
     while (stable == false)
         if (V_AC > 40)
             break;
@@ -20,6 +21,7 @@ for V_DC = 2 : step_V_DC : 4.5
         V_AC = V_AC + step_V_AC;
         stable = getTrajectoryStability(T, h, ic, V_DC, V_AC, m);
     end
+
     if (V_AC < 40)
         values(i,1) = V_AC;
         values(i,2) = V_DC;
@@ -33,6 +35,7 @@ for V_DC = 2 : step_V_DC : 4.5
         V_AC = V_AC + step_V_AC;
         stable = getTrajectoryStability(T, h, ic, V_DC, V_AC, m);
     end
+
     if (V_AC < 40)
         values(i,1) = V_AC;
         values(i,2) = V_DC;
@@ -40,22 +43,20 @@ for V_DC = 2 : step_V_DC : 4.5
     end
 
     V_AC = 10;
-
 end
 
 set(groot, 'defaultTextInterpreter', 'latex');
 set(groot, 'defaultAxesTickLabelInterpreter', 'latex');
 set(groot, 'defaultLegendInterpreter', 'latex');
 
-fs = 12; % font size
 stability = figure();
 hold on;
 plot(values(:,1), values(:,2), 'k.');
-xlabel('V_{AC}', 'FontSize', fs); ylabel('V_{DC}', 'FontSize', fs);
+xlabel('$V_{AC}$'); ylabel('$V_{DC}$');
 axis([10, 30, 2, 4]);
-annotation('textbox', [0.25,0.75,0.1,0.1], 'String', 'Unstable',...
+annotation('textbox', [0.25,0.75,0.1,0.1], 'String', 'Ustabil',...
     'EdgeColor',[1,1,1]); % text box in plot
-annotation('textbox', [0.65,0.3,0.1,0.1], 'String', 'Stable',...
+annotation('textbox', [0.65,0.3,0.1,0.1], 'String', 'Stabil',...
     'EdgeColor',[1,1,1]);
 saveTightFigure(stability, ...
             'figures/plotStabilityForNPlus.pdf'); % saves figure
